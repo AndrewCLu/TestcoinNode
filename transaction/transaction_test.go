20c773ae30ee3fb991af4744ce518b169d1a3b86
AndrewCLu <andrewclu98@gmail.com>
2021-11-22 21:45:59 -0800

    test sign verify transaction input

--- transaction/transaction_test.go
+++ transaction/transaction_test.go
@@ -3,6 +3,7 @@ package transaction
 import (
 	"testing"
 
+	"github.com/AndrewCLu/TestcoinNode/account"
 	"github.com/AndrewCLu/TestcoinNode/protocol"
 	"github.com/AndrewCLu/TestcoinNode/util"
 )
@@ -19,5 +20,21 @@ func TestTransactionToByteArray(t *testing.T) {
 
 	if !transaction.Equal(decodedTransaction) {
 		t.Fatalf(`Decoded transaction is not equal to original. Original: %v, Decoded: %v`, transaction, decodedTransaction)
+	}
+}
+
+func TestSignVerifyTransactionInput(t *testing.T) {
+	account := account.NewAccount()
+	transaction, success := NewCoinbaseTransaction(account.GetAddress(), 10)
+
+	if !success {
+		t.Fatalf(`Failed to create a new coinbase transaction.`)
+	}
+
+	signature := SignInput(account.GetPrivateKey(), transaction.Hash(), uint16(0))
+	verified := VerifyInput(account.GetPublicKey(), transaction.Hash(), uint16(0), signature)
+
+	if !verified {
+		t.Fatalf(`Failed to verify the signature of a new coinbase transaction.`)
 	}
 }
