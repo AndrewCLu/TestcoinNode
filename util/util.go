436b8fbbbfdcc268061c33389da246cdb05eafb5
AndrewCLu <andrewclu98@gmail.com>
2021-11-28 01:28:21 -0800

    comment util

--- util/util.go
+++ util/util.go
@@ -7,32 +7,7 @@ import (
 	"github.com/AndrewCLu/TestcoinNode/protocol"
 )
 
-// func AddressToHexString(bytes [protocol.AddressLength]byte) string {
-// 	return hex.EncodeToString(bytes[:])
-// }
-
-// func HexStringToAddress(str string) [protocol.AddressLength]byte {
-// 	var address [protocol.AddressLength]byte
-// 	decodedBytes, _ := hex.DecodeString(str)
-
-// 	copy(address[:], decodedBytes)
-
-// 	return address
-// }
-
-// func HashToHexString(bytes [crypto.HashLength]byte) string {
-// 	return hex.EncodeToString(bytes[:])
-// }
-
-// func HexStringToHash(str string) [crypto.HashLength]byte {
-// 	var hash [crypto.HashLength]byte
-// 	decodedBytes, _ := hex.DecodeString(str)
-
-// 	copy(hash[:], decodedBytes)
-
-// 	return hash
-// }
-
+// Concatenates a slice of byte slices into one slice
 func ConcatByteSlices(slices [][]byte) []byte {
 	var totalLen int
 	for _, s := range slices {
@@ -50,16 +25,18 @@ func ConcatByteSlices(slices [][]byte) []byte {
 }
 
 // TODO: Handle special cases where values are very large
+// Converts a human readable float representation of a Testcoin amount into a transactional amount
 func Float64UnitToUnit64Unit(value float64) uint64 {
 	return uint64(value * protocol.TestcoinUnitMultipler)
 }
 
 // TODO: Handle special cases where values are very large
+// Converts a uin64 transactional representation of a Testcoin amount innto human readable float units
 func Uint64UnitToFloat64Unit(value uint64) float64 {
 	return float64(value) / protocol.TestcoinUnitMultipler
 }
 
-// TODO: Safety checks on inputs
+// Converts a uint16 to byte representation
 func Uint16ToBytes(value uint16) []byte {
 	bytes := make([]byte, 2)
 	binary.BigEndian.PutUint16(bytes, value)
@@ -67,12 +44,12 @@ func Uint16ToBytes(value uint16) []byte {
 	return bytes
 }
 
-// TODO: Safety checks on inputs
+// Converts uint16 as bytes to a uint16
 func BytesToUint16(bytes []byte) uint16 {
 	return binary.BigEndian.Uint16(bytes)
 }
 
-// TODO: Safety checks on inputs
+// Converts a uint32 to byte representation
 func Uint32ToBytes(value uint32) []byte {
 	bytes := make([]byte, 4)
 	binary.BigEndian.PutUint32(bytes, value)
@@ -80,12 +57,12 @@ func Uint32ToBytes(value uint32) []byte {
 	return bytes
 }
 
-// TODO: Safety checks on inputs
+// Converts uint32 as bytes to a uint32
 func BytesToUint32(bytes []byte) uint32 {
 	return binary.BigEndian.Uint32(bytes)
 }
 
-// TODO: Safety checks on inputs
+// Converts a uint64 to byte representation
 func Uint64ToBytes(value uint64) []byte {
 	bytes := make([]byte, 8)
 	binary.BigEndian.PutUint64(bytes, value)
@@ -93,12 +70,12 @@ func Uint64ToBytes(value uint64) []byte {
 	return bytes
 }
 
-// TODO: Safety checks on inputs
+// Converts uint64 as bytes to a uint64
 func BytesToUint64(bytes []byte) uint64 {
 	return binary.BigEndian.Uint64(bytes)
 }
 
-// TODO: Safety checks on inputs
+// Converts a float64 to byte representation
 func Float64ToBytes(value float64) []byte {
 	bits := math.Float64bits(value)
 	bytes := make([]byte, 8)
@@ -107,7 +84,7 @@ func Float64ToBytes(value float64) []byte {
 	return bytes
 }
 
-// TODO: Safety checks on inputs
+// Converts float64 as bytes to a float64
 func BytesToFloat64(bytes []byte) float64 {
 	bits := binary.BigEndian.Uint64(bytes)
 	float := math.Float64frombits(bits)
